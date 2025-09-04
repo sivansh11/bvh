63ead19a00426c0e80a0dc5dddbd520580c367d8
sivansh11 <sivanshgupta2002@gmail.com>
2025-09-05 00:29:10 +0530

    added gpu_bvh built using tinybvh

--- includes/bvh/bvh.hpp
+++ includes/bvh/bvh.hpp
@@ -1,3 +1,40 @@
 #ifndef BVH_BVH_HPP
 #define BVH_BVH_HPP
+
+#include <cstdint>
+#include <vector>
+
+#include "math/math.hpp"
+#include "model/model.hpp"
+
+namespace bvh {
+
+struct gpu_node_t {
+  math::vec3 lmin;
+  uint32_t left;
+  math::vec3 lmax;
+  uint32_t right;
+  math::vec3 rmin;
+  uint32_t triCount;
+  math::vec3 rmax;
+  uint32_t firstTri;
+};
+static_assert(sizeof(gpu_node_t) == 64, "sizeof(node_t) should be 64 bytes");
+
+struct triangle_t {
+  math::vec4 v0;
+  math::vec4 v1;
+  math::vec4 v2;
+};
+
+struct gpu_bvh_t {
+  std::vector<gpu_node_t> nodes;
+  std::vector<triangle_t> triangles;
+  std::vector<uint32_t> indices;
+};
+
+gpu_bvh_t build_gpu_bvh(const model::raw_mesh_t &mesh);
+
+} // namespace bvh
+
 #endif
--- src/bvh.cpp
+++ src/bvh.cpp
@@ -1,1 +1,48 @@
 #include "bvh/bvh.hpp"
+#include <iostream>
+
+#define TINYBVH_USE_CUSTOM_VECTOR_TYPES
+namespace tinybvh {
+using bvhint2 = math::ivec2;
+using bvhint3 = math::ivec3;
+using bvhuint2 = math::uvec2;
+using bvhvec2 = math::vec2;
+using bvhvec3 = math::vec3;
+using bvhvec4 = math::vec4;
+using bvhdbl3 = math::dvec3;
+using bvhmat4 = math::mat4;
+} // namespace tinybvh
+#define TINYBVH_IMPLEMENTATION
+#include "tiny_bvh.h"
+
+namespace bvh {
+
+gpu_bvh_t build_gpu_bvh(const model::raw_mesh_t &mesh) {
+  gpu_bvh_t gpu_bvh{};
+  for (uint32_t i = 0; i < mesh.indices.size(); i += 3) {
+    triangle_t triangle{
+        {mesh.vertices[mesh.indices[i + 0]].position, 0},
+        {mesh.vertices[mesh.indices[i + 1]].position, 0},
+        {mesh.vertices[mesh.indices[i + 2]].position, 0},
+    };
+    gpu_bvh.triangles.push_back(triangle);
+  }
+
+  tinybvh::BVH_GPU bvh{};
+  bvh.BuildHQ(reinterpret_cast<tinybvh::bvhvec4 *>(gpu_bvh.triangles.data()),
+              static_cast<uint32_t>(gpu_bvh.triangles.size()));
+
+  gpu_bvh.nodes.resize(bvh.usedNodes);
+  static_assert(sizeof(tinybvh::BVH_GPU::BVHNode) == sizeof(gpu_node_t),
+                "sizes should be same");
+  std::memcpy(gpu_bvh.nodes.data(), bvh.bvhNode,
+              bvh.usedNodes * sizeof(gpu_node_t));
+
+  gpu_bvh.indices.resize(gpu_bvh.triangles.size());
+  std::memcpy(gpu_bvh.indices.data(), bvh.bvh.primIdx,
+              gpu_bvh.triangles.size() * sizeof(uint32_t));
+
+  return gpu_bvh;
+}
+
+} // namespace bvh
