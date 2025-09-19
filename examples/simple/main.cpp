6aeb3d121de295dfa069738015459e6d2cc5a619
sivansh11 <sivanshgupta2002@gmail.com>
2025-09-20 05:03:55 +0530

    updated traversal to traverse 32 byte nodes

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <limits>
 
 #include "bvh/bvh.hpp"
 #include "glm/common.hpp"
@@ -50,27 +51,27 @@ struct image_t {
     file.close();
   }
 
-  uint32_t _width, _height;
+  uint32_t    _width, _height;
   math::vec4 *_p_pixels;
 };
 
 class camera_t {
-public:
+ public:
   camera_t(float vfov, math::vec3 from, math::vec3 at,
            math::vec3 up = {0, 1, 0})
       : _vfov(vfov), _from(from), _at(at), _up(up) {}
 
   void set_dimentions(uint32_t width, uint32_t height) {
-    _width = width;
+    _width  = width;
     _height = height;
 
     float aspect_ratio = float(_width) / float(_height);
 
-    float focal_length = length(_from - _at);
-    float theta = math::radians(_vfov);
-    float h = tan(theta / 2.f);
+    float focal_length    = length(_from - _at);
+    float theta           = math::radians(_vfov);
+    float h               = tan(theta / 2.f);
     float viewport_height = 2 * h * focal_length;
-    float viewport_width = viewport_height * (float(_width) / float(_height));
+    float viewport_width  = viewport_height * (float(_width) / float(_height));
 
     _w = normalize(_from - _at);
     _u = normalize(cross(_up, _w));
@@ -97,10 +98,10 @@ public:
     return {_from, direction};
   }
 
-private:
+ private:
   uint32_t _width = 0, _height = 0;
 
-  float _vfov;
+  float      _vfov;
   math::vec3 _from;
   math::vec3 _at;
   math::vec3 _up;
@@ -116,79 +117,174 @@ math::vec3 random_color_from_hit(uint32_t v) {
           (((v + 1) * 789) % 255) / 255.f};
 }
 
-math::vec4 traverse(const math::vec3 O, const math::vec3 D, const math::vec3 rD,
-                    const float tmax, math::vec4 *vert, uint32_t *indices,
-                    bvh::gpu_node_t *nodes) {
-  math::vec4 hit;
-  hit.x = tmax;
-  uint node = 0, stack[64], stackPtr = 0;
-  while (true) {
-    const bvh::gpu_node_t n = nodes[node];
-    const math::vec3 lmin = n.lmin, lmax = n.lmax;
-    const math::vec3 rmin = n.rmin, rmax = n.rmax;
-    const uint triCount = n.triCount;
-    if (triCount > 0) {
-      const uint firstTri = n.firstTri;
-      for (uint i = 0; i < triCount; i++) {
-        const uint triIdx = indices[firstTri + i];
-        const uint v0 = triIdx * 3;
-        const math::vec3 vert0 = vert[v0 + 0];
-        const math::vec3 vert1 = vert[v0 + 1];
-        const math::vec3 vert2 = vert[v0 + 2];
-        const math::vec3 edge1 = vert1 - vert0;
-        const math::vec3 edge2 = vert2 - vert0;
-        const math::vec3 h = math::cross(D, edge2);
-        const float a = dot(edge1, h);
-        if (math::abs(a) < 0.0000001f)
-          continue;
-        const float f = 1 / a;
-        const math::vec3 s = O - vert0;
-        const float u = f * math::dot(s, h);
-        const math::vec3 q = math::cross(s, edge1);
-        const float v = f * math::dot(D, q);
-        if (u < 0 || v < 0 || u + v > 1)
-          continue;
-        const float d = f * dot(edge2, q);
-        if (d > 0.0f && d < hit.x)
-          hit = math::vec4(d, u, v, math::uintBitsToFloat(triIdx));
+static const uint32_t null_index = std::numeric_limits<uint32_t>::max();
+
+struct hit_t {
+  bool     did_intersect() { return prim_index != null_index; }
+  uint32_t prim_index = null_index;
+  float    t          = 1e30;
+  float    u, v;
+};
+
+struct triangle_hit_t {
+  bool  did_intersect() { return _did_intersect; }
+  float t, u, v;
+  bool  _did_intersect;
+};
+
+struct aabb_hit_t {
+  bool  did_intersect() { return tmin <= tmax; }
+  float tmin, tmax;
+};
+
+struct ray_t {
+  static ray_t create(math::vec3 origin, math::vec3 direction) {
+    ray_t ray;
+    ray.origin    = origin;
+    ray.direction = direction;
+    ray.inverse_direction =
+        math::vec3(safe_inverse(direction.x), safe_inverse(direction.y),
+                   safe_inverse(direction.z));
+    ray.tmax = 1e30;
+    ray.tmin = 0.0001;
+    return ray;
+  }
+  math::vec3 origin, direction, inverse_direction;
+  float      tmax, tmin;
+};
+
+triangle_hit_t intersect_triangle(const bvh::bvh_triangle_t triangle,
+                                  const ray_t               ray) {
+  const math::vec3 e1 = math::vec3{triangle.v0} - math::vec3{triangle.v1};
+  const math::vec3 e2 = math::vec3{triangle.v2} - math::vec3{triangle.v0};
+  const math::vec3 n  = cross(e1, e2);
+
+  const math::vec3 c           = math::vec3{triangle.v0} - ray.origin;
+  const math::vec3 r           = cross(ray.direction, c);
+  const float      inverse_det = 1.f / dot(n, ray.direction);  // could nan ?
+
+  float u = dot(r, e2) * inverse_det;
+  float v = dot(r, e1) * inverse_det;
+  float w = 1.f - u - v;
+
+  if (u >= 0 && v >= 0 && w >= 0) {
+    float t = dot(n, c) * inverse_det;
+    if (t > ray.tmin && t < ray.tmax) {
+      triangle_hit_t hit;
+      hit.t              = t;
+      hit.u              = u;
+      hit.v              = v;
+      hit._did_intersect = true;
+      return hit;
+    }
+  }
+  triangle_hit_t hit;
+  hit._did_intersect = false;
+  return hit;
+}
+
+aabb_hit_t intersect_aabb(const math::vec3 _min, const math::vec3 _max,
+                          const ray_t ray) {
+  math::vec3       tmin     = (_min - ray.origin) * ray.inverse_direction;
+  math::vec3       tmax     = (_max - ray.origin) * ray.inverse_direction;
+  const math::vec3 old_tmin = tmin;
+  const math::vec3 old_tmax = tmax;
+  tmin                      = min(old_tmin, old_tmax);
+  tmax                      = max(old_tmin, old_tmax);
+  float _tmin =
+      math::max(tmin[0], math::max(tmin[1], math::max(tmin[2], ray.tmin)));
+  float _tmax =
+      math::min(tmax[0], math::min(tmax[1], math::min(tmax[2], ray.tmax)));
+  aabb_hit_t hit = aabb_hit_t(_tmin, _tmax);
+  return hit;
+}
+
+static const uint32_t stack_size = 16;
+uint32_t              stack[stack_size];
+
+hit_t intersect_bvh(bvh::node_t *nodes, uint32_t *indices,
+                    bvh::bvh_triangle_t *triangles, ray_t ray) {
+  hit_t hit = hit_t();
+
+  uint32_t stack_top = 0;
+
+  bvh::node_t root = nodes[0];
+  if (!intersect_aabb(root.min, root.max, ray).did_intersect()) return hit;
+
+  if (root.is_leaf()) {
+    for (uint32_t i = 0; i < root.prim_count; i++) {
+      const uint32_t            triangle_index = indices[root.index + i];
+      const bvh::bvh_triangle_t triangle       = triangles[triangle_index];
+      triangle_hit_t triangle_hit = intersect_triangle(triangle, ray);
+      if (triangle_hit.did_intersect()) {
+        ray.tmax       = triangle_hit.t;
+        hit.prim_index = triangle_index;
+        hit.t          = triangle_hit.t;
+        hit.u          = triangle_hit.u;
+        hit.v          = triangle_hit.v;
       }
-      if (stackPtr == 0)
-        break;
-      node = stack[--stackPtr];
-      continue;
     }
-    uint left = n.left, right = n.right;
-    const math::vec3 t1a = (lmin - O) * rD, t2a = (lmax - O) * rD;
-    const math::vec3 t1b = (rmin - O) * rD, t2b = (rmax - O) * rD;
-    const math::vec3 minta = math::min(t1a, t2a), maxta = math::max(t1a, t2a);
-    const math::vec3 mintb = math::min(t1b, t2b), maxtb = math::max(t1b, t2b);
-    const float tmina =
-        math::max(math::max(math::max(minta.x, minta.y), minta.z), 0.f);
-    const float tminb =
-        math::max(math::max(math::max(mintb.x, mintb.y), mintb.z), 0.f);
-    const float tmaxa =
-        math::min(math::min(math::min(maxta.x, maxta.y), maxta.z), hit.x);
-    const float tmaxb =
-        math::min(math::min(math::min(maxtb.x, maxtb.y), maxtb.z), hit.x);
-    float dist1 = tmina > tmaxa ? 1e30f : tmina;
-    float dist2 = tminb > tmaxb ? 1e30f : tminb;
-    if (dist1 > dist2) {
-      float h = dist1;
-      dist1 = dist2;
-      dist2 = h;
-      uint t = left;
-      left = right;
-      right = t;
-    }
-    if (dist1 == 1e30f) {
-      if (stackPtr == 0)
-        break;
-      else
-        node = stack[--stackPtr];
+    return hit;
+  }
+
+  uint32_t current = root.index;
+
+  while (true) {
+    bvh::node_t left  = nodes[current + 0];
+    bvh::node_t right = nodes[current + 1];
+
+    aabb_hit_t left_hit  = intersect_aabb(left.min, left.max, ray);
+    aabb_hit_t right_hit = intersect_aabb(right.min, right.max, ray);
+
+    uint32_t start = 0;
+    uint32_t end   = 0;
+    if (left_hit.did_intersect() && left.is_leaf()) {
+      if (right_hit.did_intersect() && right.is_leaf()) {
+        start = left.index;
+        end   = right.index + right.prim_count;
+      } else {
+        start = left.index;
+        end   = left.index + left.prim_count;
+      }
     } else {
-      node = left;
-      if (dist2 != 1e30f)
-        stack[stackPtr++] = right;
+      if (right_hit.did_intersect() && right.is_leaf()) {
+        start = right.index;
+        end   = right.index + right.prim_count;
+      }
+    }
+    for (uint32_t index = start; index < end; index++) {
+      uint32_t                  triangle_index = indices[index];
+      const bvh::bvh_triangle_t triangle       = triangles[triangle_index];
+      triangle_hit_t triangle_hit = intersect_triangle(triangle, ray);
+      if (triangle_hit.did_intersect()) {
+        ray.tmax       = triangle_hit.t;
+        hit.prim_index = triangle_index;
+        hit.t          = triangle_hit.t;
+        hit.u          = triangle_hit.u;
+        hit.v          = triangle_hit.v;
+      }
+    }
+
+    if (left_hit.did_intersect() && !left.is_leaf()) {
+      if (right_hit.did_intersect() && !right.is_leaf()) {
+        if (stack_top >= stack_size) return hit;
+        if (left_hit.tmin <= right_hit.tmin) {
+          current            = left.index;
+          stack[stack_top++] = right.index;
+        } else {
+          current            = right.index;
+          stack[stack_top++] = left.index;
+        }
+      } else {
+        current = left.index;
+      }
+    } else {
+      if (right_hit.did_intersect() && !right.is_leaf()) {
+        current = right.index;
+      } else {
+        if (stack_top == 0) return hit;
+        current = stack[--stack_top];
+      }
     }
   }
   return hit;
@@ -201,28 +297,24 @@ int main(int argc, char **argv) {
   }
 
   model::raw_model_t model = model::load_model_from_path(argv[1]);
-  model = model::merge_meshes(model);
-  model::raw_mesh_t &mesh = model.meshes[0];
+  model                    = model::merge_meshes(model);
+  model::raw_mesh_t &mesh  = model.meshes[0];
 
-  bvh::gpu_bvh_t bvh = bvh::build_gpu_bvh(mesh);
+  bvh::bvh_t bvh = bvh::build_bvh(mesh);
 
-  image_t image{640, 420};
+  image_t  image{640, 420};
   camera_t camera{90.f, {0, 1, 2}, {0, 1, 0}};
   camera.set_dimentions(image._width, image._height);
 
   for (uint32_t y = 0; y < image._height; y++)
     for (uint32_t x = 0; x < image._width; x++) {
       auto [O, D] = camera.ray_gen(x, y);
-      math::vec3 rD = {
-          safe_inverse(D.x),
-          safe_inverse(D.y),
-          safe_inverse(D.z),
-      };
-      auto hit = traverse(O, D, rD, 1e30, (math::vec4 *)bvh.triangles.data(),
-                          bvh.indices.data(), bvh.nodes.data());
-      if (hit.x != 1e30f) {
+      ray_t ray   = ray_t::create(O, D);
+      auto  hit   = intersect_bvh(bvh.nodes.data(), bvh.prim_indices.data(),
+                                  bvh.triangles.data(), ray);
+      if (hit.did_intersect()) {
         image.at(x, image._height - y - 1) =
-            math::vec4{random_color_from_hit(math::floatBitsToUint(hit.w)), 1};
+            math::vec4{random_color_from_hit(hit.prim_index), 1};
       } else {
         image.at(x, y) = math::vec4{0, 0, 0, 0};
       }
