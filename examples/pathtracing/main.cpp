fa1d675bb71ae124c3e7583ef2aa925e7ae508e4
sivansh11 <sivanshgupta2002@gmail.com>
2026-03-23 08:59:08 +0530

    golden ratio sampler

--- examples/pathtracing/main.cpp
+++ examples/pathtracing/main.cpp
@@ -472,7 +472,7 @@ int main(int argc, char **argv) {
 
   render(16, config.max_spp, 4, image, argv[2],
          [&](uint32_t x, uint32_t y, uint32_t current_spp) -> math::vec3 {
-           sampler_t sampler = create_white_noise_sampler(
+           sampler_t sampler = create_golden_ratio_sampler(
                x, y, image._width, image._height, current_spp);
            float jitter_x = static_cast<float>(x) + (sampler.randf() - 0.5f);
            float jitter_y = static_cast<float>(y) + (sampler.randf() - 0.5f);
--- examples/pathtracing/sampler.hpp
+++ examples/pathtracing/sampler.hpp
@@ -1,6 +1,7 @@
 #ifndef SAMPLER_HPP
 #define SAMPLER_HPP
 
+#include <limits>
 #include <stdexcept>
 
 #include "math/triangle.hpp"
@@ -8,6 +9,7 @@
 enum class sampler_type_t {
   e_unknown,
   e_white_noise,
+  e_golden_ratio,
 };
 
 inline static uint32_t pcg_hash(uint32_t input) {
@@ -25,15 +27,49 @@ struct white_noise_sampler_t {
   }
 };
 
+/*
+ * golden ratio sampler needs to be random initially (blue ?), but follow golden
+ * ration in spp
+ *
+ * dimension 0 spp 0 -> random
+ * dimension 1 spp 0 -> random
+ *
+ * dimension 0 spp n -> random + n * phi
+ * dimension 1 spp n -> random + n * phi
+ *
+ */
+struct golden_ratio_sampler_t {
+  uint32_t    x;
+  uint32_t    width;
+  uint32_t    y;
+  uint32_t    height;
+  uint32_t    spp;
+  uint32_t    dimension;
+  const float phi = 0.6180339887498948482f;
+  // every randf call increases the dimension
+  // for now initial offset is white
+  float randf() {
+    // TODO: sample blue noise for initial offset
+    uint32_t pixel_index = x + (y * width);
+    uint32_t seed        = pcg_hash(pixel_index) ^ pcg_hash(dimension++);
+    float    offset      = static_cast<float>(seed) * (1.f / 4294967296.f);
+    float    v           = offset + spp * phi;
+    return v - std::floor(v);
+  }
+};
+
 struct sampler_t {
   sampler_type_t type = sampler_type_t::e_unknown;
   union as_t {
-    white_noise_sampler_t white_noise_sampler;
+    white_noise_sampler_t  white_noise_sampler;
+    golden_ratio_sampler_t golden_ratio_sampler;
   } as;
   float randf() {
     switch (type) {
       case sampler_type_t::e_white_noise:
         return as.white_noise_sampler.randf();
+      case sampler_type_t::e_golden_ratio:
+        return as.golden_ratio_sampler.randf();
       default:
         throw std::runtime_error("unknown sampler");
     }
@@ -82,9 +118,25 @@ inline sampler_t create_white_noise_sampler(uint32_t x,       //
                                             uint32_t height,  //
                                             uint32_t spp) {
   sampler_t sampler{};
-  sampler.type = sampler_type_t::e_white_noise;
-  uint32_t pixel_index = x + y * width;
+  sampler.type                         = sampler_type_t::e_white_noise;
+  uint32_t pixel_index                 = x + y * width;
   sampler.as.white_noise_sampler.state = pcg_hash(pixel_index) ^ pcg_hash(spp);
+  return sampler;
+}
+
+inline sampler_t create_golden_ratio_sampler(uint32_t x,       //
+                                             uint32_t y,       //
+                                             uint32_t width,   //
+                                             uint32_t height,  //
+                                             uint32_t spp) {
+  sampler_t sampler{};
+  sampler.type                              = sampler_type_t::e_golden_ratio;
+  sampler.as.golden_ratio_sampler.dimension = 0;
+  sampler.as.golden_ratio_sampler.x         = x;
+  sampler.as.golden_ratio_sampler.y         = y;
+  sampler.as.golden_ratio_sampler.width     = width;
+  sampler.as.golden_ratio_sampler.height    = height;
+  sampler.as.golden_ratio_sampler.spp       = spp;
   return sampler;
 }
 
